570d80586e1c6ba26951d02d68daf84bb3f086a6
mdorante <mdorante10@gmail.com>
2021-01-18 09:38:55 -0300

    add plugin system examples

---
+++ Chapter_3_Working_with_the_Command_Line/Creating_Command_Line_Tools/implementing_plugins/plugin_a.py
@@ -0,0 +1,7 @@
+'''
+simple plugin for plugin system example
+'''
+
+
+def run():
+    print('Plugin A doing some stuff!')
---
+++ Chapter_3_Working_with_the_Command_Line/Creating_Command_Line_Tools/implementing_plugins/plugin_b.py
@@ -0,0 +1,7 @@
+'''
+simple plugin for plugin system example
+'''
+
+
+def run():
+    print('Plugin B doing some other stuff!!!')
